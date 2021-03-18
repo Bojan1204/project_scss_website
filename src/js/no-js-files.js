528453efd769aaeff31ab31180a55af655cf5bc4
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2021-03-19 00:26:27 +0100

    footer is finished

--- src/js/no-js-files.js
+++ src/js/no-js-files.js
@@ -3,8 +3,8 @@
 // import '../img/webpack4.jpg';
 // import '../img/bg.png';
 import '../scss/index.scss';
-import '../img/icons/favicon.ico';
-import '../img/icons/icon.png';
+// import '../img/icons/favicon.ico';
+import '../img/icons/favicon.png';
 import '../../.gitkeep';
 import '../.htaccess';
 import '../fonts/Anton-Regular.ttf';
